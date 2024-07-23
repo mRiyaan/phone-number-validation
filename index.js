e8ac534aadfe72d3955626ecd9eccc237581a6f5
mRiyaan <mohammedriyaan115@gmail.com>
2024-07-23 15:19:19 +0530

    type added js updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-let form = document.getElementById('form');
-let fullname = document.getElementById('fullname');
-let phonenumber = document.getElementById('phoneNumber');
+const form = document.getElementById('form');
+const fullname = document.getElementById('fullname');
+const phonenumber = document.getElementById('phoneNumber');
 
-form.addEventListener('submit', e=> {
+form.addEventListener('submit', e => {
     e.preventDefualt();
 });
