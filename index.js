7d748d88ee6050cee22a601dee6e9bb65c6f5e2e
mRiyaan <mohammedriyaan115@gmail.com>
2024-07-29 19:29:17 +0530

    dhdhdh

--- index.js
+++
@@ -1,7 +0,0 @@
-const form = document.getElementById('form');
-const fullname = document.getElementById('fullname');
-const phonenumber = document.getElementById('phoneNumber');
-
-form.addEventListener('submit', e => {
-    e.preventDefualt();
-});
---
+++ validate.js
@@ -0,0 +1,53 @@
+const form = document.getElementById('.form');
+const inputControl = document.querySelector('.input-control');
+const nameError = document.querySelector('.nameError');
+const phoneError = document.querySelector('.phoneError');
+
+document.getElementById('form').addEventListener('submit',function (e){
+   e.preventDefault();
+   validateForm();
+
+});
+
+
+
+function validateFullName(){
+    const name=document.getElementById('fullName').value;
+
+    if(name.value ===''){
+        nameError.innerHTML ='Name is required';
+        
+    }
+  
+    else{
+        form.innerHTML =(action="submit.html")
+    }  
+
+}
+
+function validatePhoneNumber(){
+    const phone=document.getElementById('phonenumber').value;
+
+    if (phone.value ===''){
+        phoneError.innerHTML='Phone Number is required';
+        
+    }
+    else if(phone.length !==10){
+        phoneError.innerHTML='Enter valid 10 digit Phone Number';
+        
+    }
+    else if(phone.match(/^\d{10}$/)){
+        phoneError.innerHTML='Enter valid 10 digit Phone Number';
+
+    }
+    else {
+        form.innerHTML =(action="submit.html")
+    }
+
+}
+
+function validteForm(){
+    if(!validateFullName() || !validatePhoneNumber()){
+        return false;
+    }
+}
